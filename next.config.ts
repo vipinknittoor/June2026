11f8f1003c5de3787b0e4f1771cc99ce33d50100
Adarsh Vinod <adarsh.vinod.3762@gmail.com>
2026-08-27 11:00:17 +0530

    chore: add docker config, update api url to deployed IP

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
 };
 
 export default withPWA(nextConfig);
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,14 +9,14 @@ export interface ApiResponse<T> {
 }
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://3.109.179.178:3000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const refreshClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://3.109.179.178:3000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
